ae1a3604b1c15f97a09575c3720cbe162e68d928
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 15:43:22 +0000

    2.13.0

---
+++ publishpress-checklists/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'publishpress/publishpress-checklists',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f1f99f583999e29c25d7baa5cff4c65ffe1f2038',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'publishpress/publishpress-checklists' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f1f99f583999e29c25d7baa5cff4c65ffe1f2038',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
